b0477b31677ce9e687035b959fbf4fd2720944e0
coolgogi <suhyun926@naver.com>
2022-03-11 13:17:20 +0900

    update ddmin.sh

--- test/ddmin.sh
+++ test/ddmin.sh
@@ -3,4 +3,4 @@ echo "ddmin algorithm test"
 rm output/ddmin_output.txt
 touch output/ddmin_output.txt
 
-../bin/ddmin input/jsondump input/dd-3-5-9-2-3-4-8-7-10.json
+../bin/ddmin input/jsondump input/dd-3-5-9-2-3-4-8-7-10-16.json
