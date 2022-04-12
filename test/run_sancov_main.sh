256874fbe97bd51d585c88b90e18b5c083212652
coolgogi <coolgogi1024@gmail.com>
2022-04-12 14:19:29 +0900

    update shell script

--- test/run_sancov_main.sh
+++ test/run_sancov_main.sh
@@ -1,14 +1,25 @@
-echo "test jsondump input : corpus"
+echo "test xmllint with sancov"
 
 main="../bin/sancov_main"
 target="../libxml2/xmllint"
 input_dir="../libxml2/test/"
-output_dir="output/libxml2/"
+dir="output/libxml2/"
+output=${dir}
+if [ $1 -eq "350" ];
+then
+	output="${dir}issue350/"
+elif [ $1 -eq "243" ];
+then
+	output="${dir}issue243/"
+elif [ $1 -eq "311" ];
+then
+	output="${dir}issue311/"
+fi
 
 
-rm -r $output_dir*
-mkdir output/libxml2
-mkdir output/libxml2/pass
-mkdir output/libxml2/fail
+rm -r $output*
+mkdir ${output}pass
+mkdir ${output}fail
+mkdir ${output}tout
 ASAN_OPTIONS=coverage=1 $main $input_dir $output_dir $target
 
